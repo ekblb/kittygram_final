9ce5da639c425efc54f7eef824d2bb7dc5c98157
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-08-09 18:58:52 +0300

    Correct ALLOWED_HOSTS in env file

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS')).split(', ')
+ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost')).split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
