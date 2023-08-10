d25b268ac9bdef97a4430a27fcd60403ada8e4ac
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-08-10 14:22:54 +0300

    New commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
 
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+DEBUG = (os.getenv('DEBUG', 'True') == 'True')
 
 ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost')).split(', ')
 
